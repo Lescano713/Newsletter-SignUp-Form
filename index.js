f494300823439e149ecff57cd1b6e6964fff2de5
Lescano713 <131562199+Lescano713@users.noreply.github.com>
2024-08-02 23:55:25 -0500

    error-state

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 const mainContainer = document.getElementById('main-container');
 const infoContainer = document.querySelector('.info-container');
-console.log(window.innerWidth);
+const inputEmail = document.querySelector('input');
+const button = document.querySelector('button[type="submit"]');
+const form = document.querySelector('form');
+
+
+button.addEventListener('click', e =>{
+    e.preventDefault();
+    if (!inputEmail.value.search("@gmail.com") ) {
+        form.classList.add('error-state');
+        console.log(inputEmail.value)
+    }else{
+        mainContainer.style.display = 'none';
+        console.log("enviado")
+    }
+
+});
+
+// function errorState(){
+//     const label = inputEmail.value;
+//     if (label == "a") {
+//         inputEmail.classList.add('error-state');
+//     }else{
+//         console.log("no agarra")
+//     }
+// }
+// function show(){
+//     form.classList.add('error-state')
+//     form.style.backgroundColor = "red";
+    
+// }
